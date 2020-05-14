6568449d9ae2da8b1dc473b08f070ed8d3fe48c9
Juan Carlos Aguirre <49243335+JamesCharles-AC@users.noreply.github.com>
2020-05-14 18:02:28 +0200

    Main de la aplicación actualizado
    Más problemas relacionados con el push desactualizado

---
+++ src/modelo/Main_App.java
@@ -0,0 +1,60 @@
+/*
+ * 
+ */
+package modelo;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Login.
+ */
+public class Main_App extends Application {
+	
+	/**
+	 * Start.
+	 *
+	 * @param primaryStage the primary stage
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+
+		try {
+
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main_App.class.getResource("/vista/login.fxml"));
+			// Cargo la ventana
+			Pane ventana = (Pane) loader.load();
+
+			// Cargo el scene
+			Scene scene = new Scene(ventana);
+			scene.getStylesheets().add("/vista/login.css");
+			
+			// Seteo la scene y la muestro
+			primaryStage.setResizable(false);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (IOException e) {
+
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+
+}
