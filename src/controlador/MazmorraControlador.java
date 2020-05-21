46e265cdf0b83a46e68adb065ca9c1eafbf41817
Juanca_Aguirre <juancarlos.aguirrecortes@gmail.com>
2020-05-21 17:57:09 +0200

    Estructura basica de los elementos

--- src/controlador/MazmorraControlador.java
+++ src/controlador/MazmorraControlador.java
@@ -1,10 +1,12 @@
 package controlador;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import modelo.criaturas.Criatura;
 import modelo.personajes.Personaje;
 
 public class MazmorraControlador {
@@ -16,25 +18,43 @@ public class MazmorraControlador {
     @FXML private Label vidaPJ;
     @FXML private Label defensaPJ;
     @FXML private Label danioPJ;
-    @FXML private Label DestrezaPJ;
-    @FXML private Label habilidadPJ;
-    @FXML private Label InteligenciaPJ;
+    @FXML private Label destrezaPJ;
+    @FXML private Label inteligenciaPJ;
     @FXML private VBox panelCriatura;
-    @FXML private Label vidacCriatura;
+    @FXML private Label vidaCriatura;
     @FXML private Label defensaCriatura;
     @FXML private Label danioCriatura;
-    @FXML private Label DestrezaCriatura;
-    @FXML private Label habilidadCriatura;
-    @FXML  private Label InteligenciaCriatura;
 
     
     private Personaje personaje;
+    private Criatura criatura;
+    
+
+
+	@FXML
+	void initialize(){
+
+		
+		/* 			Ejecutar cosas en último lugar
+		 * 
+		 * Necesario para que la criatura pasada como parametro en 
+		 * Personaje.combatir() pueda ser procesada correctamente.
+		 */
+		
+		Platform.runLater(() -> {
+
+			this.vidaPJ.setText( Integer.toString( this.personaje.getVida() ) );
+			this.defensaPJ.setText( Integer.toString( this.personaje.getDefensa() ) );
+			this.danioPJ.setText( Integer.toString( this.personaje.getDanio() ) );
+			this.destrezaPJ.setText( Integer.toString( this.personaje.getDestreza() ) );
+			this.inteligenciaPJ.setText( Integer.toString( this.personaje.getInteligencia() ) );
+	    });
+	}
     
     @FXML
     void atacar(ActionEvent event) {
 
     	this.personaje.atacar(this.criatura);
-		this.barraCriatura.setProgress( this.criatura.getVida() );
 		this.vidaCriatura.setText( Integer.toString( this.criatura.getVida() ) );
     	
 //    	Random r = new Random();
@@ -42,8 +62,7 @@ public class MazmorraControlador {
 //    	Thread.sleep(random);
     	
     	this.criatura.atacar(this.personaje);
-		this.barraPJ.setProgress( this.personaje.getVida() );
-		this.vidaPersonaje.setText( Integer.toString( this.personaje.getVida() ) );
+		this.vidaPJ.setText( Integer.toString( this.personaje.getVida() ) );
 		
 		if (this.personaje.getVida() <= 0) huir(event);
     }
@@ -61,6 +80,18 @@ public class MazmorraControlador {
     @FXML
     void huir(ActionEvent event) {
 
+    }
+    
+
+    
+    public void setCriatura(Criatura c) {
+    	
+    	this.criatura = c;
+    }
+    
+    public void setPersonaje(Personaje p) {
+    	
+    	this.personaje = p;
     }
 
 }
