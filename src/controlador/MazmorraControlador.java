f632263d39e0df134fd7d6a4173de2780dd8cb7d
chris <chris@DESKTOP-FIHGIQD>
2020-05-28 23:25:01 +0200

    Añadido el movimiento del pj
    Añadida las funcionas de crear y borrar la img del pj en el mapa

--- src/controlador/MazmorraControlador.java
+++ src/controlador/MazmorraControlador.java
@@ -6,13 +6,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
-
-import com.sun.prism.paint.Color;
-
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -59,209 +55,208 @@ public class MazmorraControlador {
 	private Pane imagenMapa;
 	@FXML
 	private GridPane gridMovimiento;
-    @FXML
-    private ImageView mazmorraMap;
-    @FXML
-    private ImageView c91;
-    @FXML
-    private ImageView c2;
-    @FXML
-    private ImageView c3;
-    @FXML
-    private ImageView c4;
-    @FXML
-    private ImageView c5;
-    @FXML
-    private ImageView c6;
-    @FXML
-    private ImageView c7;
-    @FXML
-    private ImageView c8;
-    @FXML
-    private ImageView c9;
-    @FXML
-    private ImageView c10;
-    @FXML
-    private ImageView c12;
-    @FXML
-    private ImageView c11;
-    @FXML
-    private ImageView c13;
-    @FXML
-    private ImageView c14;
-    @FXML
-    private ImageView c15;
-    @FXML
-    private ImageView c16;
-    @FXML
-    private ImageView c17;
-    @FXML
-    private ImageView c18;
-    @FXML
-    private ImageView c19;
-    @FXML
-    private ImageView c20;
-    @FXML
-    private ImageView c21;
-    @FXML
-    private ImageView c22;
-    @FXML
-    private ImageView c23;
-    @FXML
-    private ImageView c24;
-    @FXML
-    private ImageView c25;
-    @FXML
-    private ImageView c26;
-    @FXML
-    private ImageView c27;
-    @FXML
-    private ImageView c28;
-    @FXML
-    private ImageView c29;
-    @FXML
-    private ImageView c30;
-    @FXML
-    private ImageView c32;
-    @FXML
-    private ImageView c31;
-    @FXML
-    private ImageView c81;
-    @FXML
-    private ImageView c71;
-    @FXML
-    private ImageView c61;
-    @FXML
-    private ImageView c51;
-    @FXML
-    private ImageView c44;
-    @FXML
-    private ImageView c43;
-    @FXML
-    private ImageView c42;
-    @FXML
-    private ImageView c41;
-    @FXML
-    private ImageView c33;
-    @FXML
-    private ImageView c75;
-    @FXML
-    private ImageView c65;
-    @FXML
-    private ImageView c54;
-    @FXML
-    private ImageView c64;
-    @FXML
-    private ImageView c74;
-    @FXML
-    private ImageView c84;
-    @FXML
-    private ImageView c83;
-    @FXML
-    private ImageView c73;
-    @FXML
-    private ImageView c63;
-    @FXML
-    private ImageView c53;
-    @FXML
-    private ImageView c82;
-    @FXML
-    private ImageView c72;
-    @FXML
-    private ImageView c62;
-    @FXML
-    private ImageView c52;
-    @FXML
-    private ImageView c46;
-    @FXML
-    private ImageView c45;
-    @FXML
-    private ImageView c55;
-    @FXML
-    private ImageView c35;
-    @FXML
-    private ImageView c94;
-    @FXML
-    private ImageView c93;
-    @FXML
-    private ImageView c92;
-    @FXML
-    private ImageView c34;
-    @FXML
-    private ImageView c69;
-    @FXML
-    private ImageView c59;
-    @FXML
-    private ImageView c78;
-    @FXML
-    private ImageView c76;
-    @FXML
-    private ImageView c67;
-    @FXML
-    private ImageView c66;
-    @FXML
-    private ImageView c57;
-    @FXML
-    private ImageView c47;
-    @FXML
-    private ImageView c37;
-    @FXML
-    private ImageView c36;
-    @FXML
-    private ImageView c56;
-    @FXML
-    private ImageView c97;
-    @FXML
-    private ImageView c96;
-    @FXML
-    private ImageView c95;
-    @FXML
-    private ImageView c87;
-    @FXML
-    private ImageView c86;
-    @FXML
-    private ImageView c85;
-    @FXML
-    private ImageView c77;
-    @FXML
-    private ImageView c68;
-    @FXML
-    private ImageView c58;
-    @FXML
-    private ImageView c49;
-    @FXML
-    private ImageView c48;
-    @FXML
-    private ImageView c40;
-    @FXML
-    private ImageView c39;
-    @FXML
-    private ImageView c38;
-    @FXML
-    private ImageView c79;
-    @FXML
-    private ImageView c60;
-    @FXML
-    private ImageView c50;
-    @FXML
-    private ImageView c70;
-    @FXML
-    private ImageView c88;
-    @FXML
-    private ImageView c99;
-    @FXML
-    private ImageView c100;
-    @FXML
-    private ImageView c90;
-    @FXML
-    private ImageView c89;
-    @FXML
-    private ImageView c80;
-    @FXML
-    private ImageView c1;
-    @FXML
-    private ImageView c98;
-
+	@FXML
+	private ImageView mazmorraMap;
+	@FXML
+	private ImageView c91;
+	@FXML
+	private ImageView c2;
+	@FXML
+	private ImageView c3;
+	@FXML
+	private ImageView c4;
+	@FXML
+	private ImageView c5;
+	@FXML
+	private ImageView c6;
+	@FXML
+	private ImageView c7;
+	@FXML
+	private ImageView c8;
+	@FXML
+	private ImageView c9;
+	@FXML
+	private ImageView c10;
+	@FXML
+	private ImageView c12;
+	@FXML
+	private ImageView c11;
+	@FXML
+	private ImageView c13;
+	@FXML
+	private ImageView c14;
+	@FXML
+	private ImageView c15;
+	@FXML
+	private ImageView c16;
+	@FXML
+	private ImageView c17;
+	@FXML
+	private ImageView c18;
+	@FXML
+	private ImageView c19;
+	@FXML
+	private ImageView c20;
+	@FXML
+	private ImageView c21;
+	@FXML
+	private ImageView c22;
+	@FXML
+	private ImageView c23;
+	@FXML
+	private ImageView c24;
+	@FXML
+	private ImageView c25;
+	@FXML
+	private ImageView c26;
+	@FXML
+	private ImageView c27;
+	@FXML
+	private ImageView c28;
+	@FXML
+	private ImageView c29;
+	@FXML
+	private ImageView c30;
+	@FXML
+	private ImageView c32;
+	@FXML
+	private ImageView c31;
+	@FXML
+	private ImageView c81;
+	@FXML
+	private ImageView c71;
+	@FXML
+	private ImageView c61;
+	@FXML
+	private ImageView c51;
+	@FXML
+	private ImageView c44;
+	@FXML
+	private ImageView c43;
+	@FXML
+	private ImageView c42;
+	@FXML
+	private ImageView c41;
+	@FXML
+	private ImageView c33;
+	@FXML
+	private ImageView c75;
+	@FXML
+	private ImageView c65;
+	@FXML
+	private ImageView c54;
+	@FXML
+	private ImageView c64;
+	@FXML
+	private ImageView c74;
+	@FXML
+	private ImageView c84;
+	@FXML
+	private ImageView c83;
+	@FXML
+	private ImageView c73;
+	@FXML
+	private ImageView c63;
+	@FXML
+	private ImageView c53;
+	@FXML
+	private ImageView c82;
+	@FXML
+	private ImageView c72;
+	@FXML
+	private ImageView c62;
+	@FXML
+	private ImageView c52;
+	@FXML
+	private ImageView c46;
+	@FXML
+	private ImageView c45;
+	@FXML
+	private ImageView c55;
+	@FXML
+	private ImageView c35;
+	@FXML
+	private ImageView c94;
+	@FXML
+	private ImageView c93;
+	@FXML
+	private ImageView c92;
+	@FXML
+	private ImageView c34;
+	@FXML
+	private ImageView c69;
+	@FXML
+	private ImageView c59;
+	@FXML
+	private ImageView c78;
+	@FXML
+	private ImageView c76;
+	@FXML
+	private ImageView c67;
+	@FXML
+	private ImageView c66;
+	@FXML
+	private ImageView c57;
+	@FXML
+	private ImageView c47;
+	@FXML
+	private ImageView c37;
+	@FXML
+	private ImageView c36;
+	@FXML
+	private ImageView c56;
+	@FXML
+	private ImageView c97;
+	@FXML
+	private ImageView c96;
+	@FXML
+	private ImageView c95;
+	@FXML
+	private ImageView c87;
+	@FXML
+	private ImageView c86;
+	@FXML
+	private ImageView c85;
+	@FXML
+	private ImageView c77;
+	@FXML
+	private ImageView c68;
+	@FXML
+	private ImageView c58;
+	@FXML
+	private ImageView c49;
+	@FXML
+	private ImageView c48;
+	@FXML
+	private ImageView c40;
+	@FXML
+	private ImageView c39;
+	@FXML
+	private ImageView c38;
+	@FXML
+	private ImageView c79;
+	@FXML
+	private ImageView c60;
+	@FXML
+	private ImageView c50;
+	@FXML
+	private ImageView c70;
+	@FXML
+	private ImageView c88;
+	@FXML
+	private ImageView c99;
+	@FXML
+	private ImageView c100;
+	@FXML
+	private ImageView c90;
+	@FXML
+	private ImageView c89;
+	@FXML
+	private ImageView c80;
+	@FXML
+	private ImageView c1;
+	@FXML
+	private ImageView c98;
 
 	private Personaje personaje;
 	private Criatura criatura;
@@ -270,15 +265,17 @@ public class MazmorraControlador {
 	String nivelMapa;
 	Date time;
 	private int casillaActual;
-	
-	private ArrayList<ImageView> ListaCasillas = new ArrayList<ImageView>();
+	Mazmorra m;
+
+	private ArrayList<ImageView> listaImagenesCasillas = new ArrayList<ImageView>();
 
 	@FXML
 	void initialize() {
-
-		// Creo la ID del mapa, autoincremental
-
 		r1 = new Random();
+
+		/*
+		 * La ID del mapa sera el los milisegundos actuales + la zona horaria
+		 */
 		time = new Date();
 		/*
 		 * Ejecutar cosas en último lugar
@@ -289,10 +286,15 @@ public class MazmorraControlador {
 
 		Platform.runLater(() -> {
 
+			this.rellenarArray();
 			this.setStats();
 
-			// El nivel del mapa sera minimo del nivel del personaje, y aleatoriamente
-			// podria ser 1 o 2 niveles mayor que el
+			/*
+			 * El nivel del mapa sera minimo del nivel del personaje, y aleatoriamente
+			 * podria ser 1 o 2 niveles mayor que el
+			 */
+			// TODO Acordarnos de descomentar esta linea para que se generen del nivel del
+			// PJ
 //			this.nivelMapa = Integer.toString(this.personaje.getNivel() + r1.nextInt(3));
 			this.nivelMapa = Integer.toString(50 + r1.nextInt(3));
 
@@ -302,7 +304,7 @@ public class MazmorraControlador {
 			this.setMazmorra();
 
 			this.casillaActual = this.n.getMazmorra().getInicio();
-
+			m = this.n.getMazmorra();
 		});
 	}
 
@@ -365,18 +367,12 @@ public class MazmorraControlador {
 
 	private void setMazmorra() {
 
-		Mazmorra m = this.n.getMazmorra();
 		ArrayList<Casilla> casillas = m.getListaCasillas();
 		ArrayList<Integer> ocupado = new ArrayList<Integer>();
 		Casilla cas;
 
-		Image image = new Image("http://khoyac.es/JuegoMolon/img/" + m.getId() + ".png");
-		mazmorraMap.setImage(image);
-		
-		Image ninotet = new Image("/imagenes/assets/ninotet.png");
-		
-		
-		ListaCasillas.get(m.getInicio()).setImage(ninotet);
+		crearNinotet();
+
 		// imagenMapa.setStyle("-fx-background-image:
 		// url(\"http://khoyac.es/JuegoMolon/img/"+m.getId()+".png \")");
 		// imagenMapa.setStyle("-fx-background-size: cover");
@@ -458,109 +454,152 @@ public class MazmorraControlador {
 		return dateFormat.format(fecha).replace("+", "");
 
 	}
-	
+
 	private void rellenarArray() {
-		ListaCasillas.add(c1);
-		ListaCasillas.add(c1);
-		ListaCasillas.add(2, c2);
-		ListaCasillas.add(3, c3);
-		ListaCasillas.add(4, c4);
-		ListaCasillas.add(5, c5);
-		ListaCasillas.add(6, c6);
-		ListaCasillas.add(7, c7);
-		ListaCasillas.add(8, c8);
-		ListaCasillas.add(9, c9);
-		ListaCasillas.add(10, c10);
-		ListaCasillas.add(11, c11);
-		ListaCasillas.add(12, c12);
-		ListaCasillas.add(13, c13);
-		ListaCasillas.add(14, c14);
-		ListaCasillas.add(15, c15);
-		ListaCasillas.add(16, c16);
-		ListaCasillas.add(17, c17);
-		ListaCasillas.add(18, c18);
-		ListaCasillas.add(19, c19);
-		ListaCasillas.add(20, c20);
-		ListaCasillas.add(21, c21);
-		ListaCasillas.add(22, c22);
-		ListaCasillas.add(23, c23);
-		ListaCasillas.add(24, c24);
-		ListaCasillas.add(25, c25);
-		ListaCasillas.add(26, c26);
-		ListaCasillas.add(27, c27);
-		ListaCasillas.add(28, c28);
-		ListaCasillas.add(29, c29);
-		ListaCasillas.add(30, c30);
-		ListaCasillas.add(31, c31);
-		ListaCasillas.add(32, c32);
-		ListaCasillas.add(33, c33);
-		ListaCasillas.add(34, c34);
-		ListaCasillas.add(35, c35);
-		ListaCasillas.add(36, c36);
-		ListaCasillas.add(37, c37);
-		ListaCasillas.add(38, c38);
-		ListaCasillas.add(39, c39);
-		ListaCasillas.add(40, c40);
-		ListaCasillas.add(41, c41);
-		ListaCasillas.add(42, c42);
-		ListaCasillas.add(43, c43);
-		ListaCasillas.add(44, c44);
-		ListaCasillas.add(45, c45);
-		ListaCasillas.add(46, c46);
-		ListaCasillas.add(47, c47);
-		ListaCasillas.add(48, c48);
-		ListaCasillas.add(49, c49);
-		ListaCasillas.add(50, c50);
-		ListaCasillas.add(51, c51);
-		ListaCasillas.add(52, c52);
-		ListaCasillas.add(53, c53);
-		ListaCasillas.add(54, c54);
-		ListaCasillas.add(55, c55);
-		ListaCasillas.add(56, c56);
-		ListaCasillas.add(57, c57);
-		ListaCasillas.add(58, c58);
-		ListaCasillas.add(59, c59);
-		ListaCasillas.add(60, c60);
-		ListaCasillas.add(61, c61);
-		ListaCasillas.add(62, c62);
-		ListaCasillas.add(63, c63);
-		ListaCasillas.add(64, c64);
-		ListaCasillas.add(65, c65);
-		ListaCasillas.add(66, c66);
-		ListaCasillas.add(67, c67);
-		ListaCasillas.add(68, c68);
-		ListaCasillas.add(69, c69);
-		ListaCasillas.add(70, c70);
-		ListaCasillas.add(71, c71);
-		ListaCasillas.add(72, c72);
-		ListaCasillas.add(73, c73);
-		ListaCasillas.add(74, c74);
-		ListaCasillas.add(75, c75);
-		ListaCasillas.add(76, c76);
-		ListaCasillas.add(77, c77);
-		ListaCasillas.add(78, c78);
-		ListaCasillas.add(79, c79);
-		ListaCasillas.add(80, c80);
-		ListaCasillas.add(81, c81);
-		ListaCasillas.add(82, c82);
-		ListaCasillas.add(83, c83);
-		ListaCasillas.add(84, c84);
-		ListaCasillas.add(85, c85);
-		ListaCasillas.add(86, c86);
-		ListaCasillas.add(87, c87);
-		ListaCasillas.add(88, c88);
-		ListaCasillas.add(89, c89);
-		ListaCasillas.add(90, c90);
-		ListaCasillas.add(91, c91);
-		ListaCasillas.add(92, c92);
-		ListaCasillas.add(93, c93);
-		ListaCasillas.add(94, c94);
-		ListaCasillas.add(95, c95);
-		ListaCasillas.add(96, c96);
-		ListaCasillas.add(97, c97);
-		ListaCasillas.add(98, c98);
-		ListaCasillas.add(99, c99);
-		ListaCasillas.add(100, c100);
+		listaImagenesCasillas.add(c1);
+		listaImagenesCasillas.add(c1);
+		listaImagenesCasillas.add(2, c2);
+		listaImagenesCasillas.add(3, c3);
+		listaImagenesCasillas.add(4, c4);
+		listaImagenesCasillas.add(5, c5);
+		listaImagenesCasillas.add(6, c6);
+		listaImagenesCasillas.add(7, c7);
+		listaImagenesCasillas.add(8, c8);
+		listaImagenesCasillas.add(9, c9);
+		listaImagenesCasillas.add(10, c10);
+		listaImagenesCasillas.add(11, c11);
+		listaImagenesCasillas.add(12, c12);
+		listaImagenesCasillas.add(13, c13);
+		listaImagenesCasillas.add(14, c14);
+		listaImagenesCasillas.add(15, c15);
+		listaImagenesCasillas.add(16, c16);
+		listaImagenesCasillas.add(17, c17);
+		listaImagenesCasillas.add(18, c18);
+		listaImagenesCasillas.add(19, c19);
+		listaImagenesCasillas.add(20, c20);
+		listaImagenesCasillas.add(21, c21);
+		listaImagenesCasillas.add(22, c22);
+		listaImagenesCasillas.add(23, c23);
+		listaImagenesCasillas.add(24, c24);
+		listaImagenesCasillas.add(25, c25);
+		listaImagenesCasillas.add(26, c26);
+		listaImagenesCasillas.add(27, c27);
+		listaImagenesCasillas.add(28, c28);
+		listaImagenesCasillas.add(29, c29);
+		listaImagenesCasillas.add(30, c30);
+		listaImagenesCasillas.add(31, c31);
+		listaImagenesCasillas.add(32, c32);
+		listaImagenesCasillas.add(33, c33);
+		listaImagenesCasillas.add(34, c34);
+		listaImagenesCasillas.add(35, c35);
+		listaImagenesCasillas.add(36, c36);
+		listaImagenesCasillas.add(37, c37);
+		listaImagenesCasillas.add(38, c38);
+		listaImagenesCasillas.add(39, c39);
+		listaImagenesCasillas.add(40, c40);
+		listaImagenesCasillas.add(41, c41);
+		listaImagenesCasillas.add(42, c42);
+		listaImagenesCasillas.add(43, c43);
+		listaImagenesCasillas.add(44, c44);
+		listaImagenesCasillas.add(45, c45);
+		listaImagenesCasillas.add(46, c46);
+		listaImagenesCasillas.add(47, c47);
+		listaImagenesCasillas.add(48, c48);
+		listaImagenesCasillas.add(49, c49);
+		listaImagenesCasillas.add(50, c50);
+		listaImagenesCasillas.add(51, c51);
+		listaImagenesCasillas.add(52, c52);
+		listaImagenesCasillas.add(53, c53);
+		listaImagenesCasillas.add(54, c54);
+		listaImagenesCasillas.add(55, c55);
+		listaImagenesCasillas.add(56, c56);
+		listaImagenesCasillas.add(57, c57);
+		listaImagenesCasillas.add(58, c58);
+		listaImagenesCasillas.add(59, c59);
+		listaImagenesCasillas.add(60, c60);
+		listaImagenesCasillas.add(61, c61);
+		listaImagenesCasillas.add(62, c62);
+		listaImagenesCasillas.add(63, c63);
+		listaImagenesCasillas.add(64, c64);
+		listaImagenesCasillas.add(65, c65);
+		listaImagenesCasillas.add(66, c66);
+		listaImagenesCasillas.add(67, c67);
+		listaImagenesCasillas.add(68, c68);
+		listaImagenesCasillas.add(69, c69);
+		listaImagenesCasillas.add(70, c70);
+		listaImagenesCasillas.add(71, c71);
+		listaImagenesCasillas.add(72, c72);
+		listaImagenesCasillas.add(73, c73);
+		listaImagenesCasillas.add(74, c74);
+		listaImagenesCasillas.add(75, c75);
+		listaImagenesCasillas.add(76, c76);
+		listaImagenesCasillas.add(77, c77);
+		listaImagenesCasillas.add(78, c78);
+		listaImagenesCasillas.add(79, c79);
+		listaImagenesCasillas.add(80, c80);
+		listaImagenesCasillas.add(81, c81);
+		listaImagenesCasillas.add(82, c82);
+		listaImagenesCasillas.add(83, c83);
+		listaImagenesCasillas.add(84, c84);
+		listaImagenesCasillas.add(85, c85);
+		listaImagenesCasillas.add(86, c86);
+		listaImagenesCasillas.add(87, c87);
+		listaImagenesCasillas.add(88, c88);
+		listaImagenesCasillas.add(89, c89);
+		listaImagenesCasillas.add(90, c90);
+		listaImagenesCasillas.add(91, c91);
+		listaImagenesCasillas.add(92, c92);
+		listaImagenesCasillas.add(93, c93);
+		listaImagenesCasillas.add(94, c94);
+		listaImagenesCasillas.add(95, c95);
+		listaImagenesCasillas.add(96, c96);
+		listaImagenesCasillas.add(97, c97);
+		listaImagenesCasillas.add(98, c98);
+		listaImagenesCasillas.add(99, c99);
+		listaImagenesCasillas.add(100, c100);
+	}
+
+	private void moverPersonaje(String direccion) {
+
+		switch (direccion) {
+		case "arriba":
+			this.casillaActual -= 10;
+			break;
+		case "abajo":
+			this.casillaActual += 10;
+
+			break;
+		case "derecha":
+			this.casillaActual += 1;
+
+			break;
+		case "izquierda":
+			this.casillaActual -= 1;
+
+			break;
+		default:
+			break;
+		}
+
+		borrarNinotet();
+		crearNinotet();
+
+	}
+
+	private void crearNinotet() {
+		Image image = new Image("http://khoyac.es/JuegoMolon/img/" + this.m.getId() + ".png");
+		mazmorraMap.setImage(image);
+
+		Image ninotet = new Image("/imagenes/assets/ninotet.png");
+
+		listaImagenesCasillas.get(this.casillaActual).setImage(ninotet);
+	}
+
+	private void borrarNinotet() {
+
+		listaImagenesCasillas.get(this.casillaActual).setVisible(false);
+		;
+
 	}
 
 }
